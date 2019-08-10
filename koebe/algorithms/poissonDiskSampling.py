730c35e62be054a2a1d5b7f9f32cc34e95723105
John Bowers <bowersjc@jmu.edu>
2019-08-10 08:10:11 -0400

    Added Poisson disk sampling.

--- koebe/algorithms/poissonDiskSampling.py
+++ koebe/algorithms/poissonDiskSampling.py
@@ -1,13 +1,59 @@
 from typing import List
 
 import random
+import math
 
 def _distSq(x1, y1, x2, y2):
     dx = x2 - x1
     dy = y2 - y1
     return dx * dx + dy * dy
 
-def slowUniformDartThrowing(radius: float, stop_count: int = 100, initial_samples = []):
+def slowAdaptiveDartThrowing(radius_function, stop_count: int = 100, initial_samples = []):
+    
+    samples = initial_samples
+    
+    fail_count = 0
+    while fail_count < stop_count: 
+        x, y = random.random(), random.random()
+        r = radius_function(x, y)
+        for sx, sy, sr in samples:
+            rad = max(sr, r)
+            if _distSq(x, y, sx, sy) <= rad * rad:
+                fail_count += 1
+                break
+        else:
+            fail_count = 0
+            samples.append((x,y,r))
+        
+    return samples
+
+def slowAdaptiveDartThrowingWithBoundary(radius_function, stop_count: int = 100):
+    
+    # First do a 1D circular sampling
+    samples = []
+    
+    fail_count = 0
+    while fail_count < stop_count:
+        s = random.random() * 4
+        
+        x, y = ((s, 0) if s <= 1.0 
+                else (1, s - 1) if s <= 2.0
+                else (3 - s, 1) if s <= 3.0
+                else (0, 4 - s))
+        r = radius_function(x, y)
+        
+        for sx, sy, sr in samples:
+            rad = max(sr, r)
+            if _distSq(x, y, sx, sy) <= rad * rad:
+                fail_count += 1
+                break
+        else:
+            fail_count = 0
+            samples.append((x, y, r))
+    
+    return slowAdaptiveDartThrowing(radius_function, stop_count, samples)
+
+def slowUniformDartThrowing(radius: float, stop_count: int = 500, initial_samples = []):
     
     samples = initial_samples
     radiusSq = radius * radius
@@ -25,7 +71,7 @@ def slowUniformDartThrowing(radius: float, stop_count: int = 100, initial_sample
     
     return samples
 
-def slowUniformDartThrowingWithBoundary(radius: float, stop_count: int = 100):
+def slowUniformDartThrowingWithBoundary(radius: float, stop_count: int = 500):
     
     # First do a 1D circular sampling
     samples = []
