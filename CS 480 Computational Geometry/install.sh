309cee11828258bd0ab8d0b3ee992bda269ceb1a
John Bowers <johnchristopherbowers+github@gmail.com>
2020-01-15 08:34:02 -0500

    Added an installation script for JMU CS lab machines to get things up and running for students in CS 480.

---
+++ CS 480 Computational Geometry/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+
+# This script loads the necessary python packages into pip's user space
+# for use with the James Madison University computer science lab machines
+# which do not allow students to install python packages with administrative
+# access. 
+#
+# It also enables the widgetsnbextension which is necessary to get the
+# jupyter widgets running properly on the lab machines.
+
+pip3 install --user wheel
+pip3 install --user ipycanvas ipywidgets ipyevents
+
+jupyter nbextension enable --py widgetsnbextension
